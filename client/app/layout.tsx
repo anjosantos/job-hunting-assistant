8b89b5fcaf4fe1f821ef856a19fa946e31dedecd
Anjo Santos <anjosantos92@gmail.com>
2025-09-05 14:51:18 -0700

    Use TailAdmin library

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import { ApolloWrapper } from "./ApolloWrapper";
+import { SidebarProvider } from "@/context/SidebarContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +31,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ApolloWrapper>{children}</ApolloWrapper>
+        <ThemeProvider>
+          <SidebarProvider>
+            <ApolloWrapper>{children}</ApolloWrapper>
+          </SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
