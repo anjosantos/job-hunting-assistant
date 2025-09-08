e24d9936a93baf59e8002a5bb70ae71661fe467f
Anjo Santos <anjosantos92@gmail.com>
2025-09-07 21:19:54 -0700

    Make sure generate cover letter works

--- client/app/(admin)/(job-hunting)/generate-cover-letter/page.tsx
+++ client/app/(admin)/(job-hunting)/generate-cover-letter/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 import { useChat } from "@ai-sdk/react";
 import { DefaultChatTransport } from "ai";
-import { useState } from "react";
+import Markdown from "react-markdown";
+
 import Label from "@/components/form/Label";
 import Select from "@/components/form/Select";
 import Button from "@/components/ui/button/Button";
+import ComponentCard from "@/components/common/ComponentCard";
 import { ChevronDownIcon } from "@/icons";
 
 import { type Resume, type Job, type CoverLetterTemplate } from "@/types";
@@ -101,108 +103,118 @@ const GenerateCoverLetterPage = () => {
 
   return (
     <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
-      {messages
-        .filter((message) => message.role !== "user")
-        .map((message) => (
-          <div>
-            {message.parts.map((part, index) =>
-              part.type === "text" ? <span key={index}>{part.text}</span> : null
-            )}
-          </div>
-        ))}
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (input.trim()) {
-            sendMessage({ text: input });
-            setInput("");
-          }
-        }}
-      >
-        <div className="space-y-4">
-          <div>
-            <Label>Select Job</Label>
-            {getJobsData && getJobsData.getJobs ? (
-              <div className="relative">
-                <Select
-                  options={getJobsData.getJobs.map((job: Job) => ({
-                    value: job.description,
-                    label: `${job.company} - ${job.role}`,
-                  }))}
-                  placeholder="Select a job"
-                  value={jobDescription || ""}
-                  onChange={(value: string) => {
-                    setJobDescription(value);
-                  }}
-                  className="dark:bg-dark-900"
-                />
-                <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-                  <ChevronDownIcon />
-                </span>
-              </div>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
-          <div>
-            <Label>Select Resume</Label>
-            {getResumesData && getResumesData.getResumes ? (
-              <div className="relative">
-                <Select
-                  options={getResumesData.getResumes.map((resume: Resume) => ({
-                    value: resume.content,
-                    label: `${resume.dateCreated}-v${resume.version}`,
-                  }))}
-                  placeholder="Select a resume"
-                  value={resumeContent || ""}
-                  onChange={(value: string) => {
-                    setResumeContent(value);
-                  }}
-                  className="dark:bg-dark-900"
-                />
-                <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-                  <ChevronDownIcon />
-                </span>
-              </div>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
-          <div>
-            <Label>Select Cover Letter Template</Label>
-            {getCoverLetterTemplatesData &&
-            getCoverLetterTemplatesData.getCoverLetterTemplates ? (
-              <div className="relative">
-                <Select
-                  options={getCoverLetterTemplatesData.getCoverLetterTemplates.map(
-                    (coverLetterTemplate: CoverLetterTemplate) => ({
-                      value: coverLetterTemplate.content,
-                      label: coverLetterTemplate.title,
-                    })
-                  )}
-                  placeholder="Select a cover letter template"
-                  value={coverLetterTemplateContent || ""}
-                  onChange={(value: string) => {
-                    setCoverLetterTemplateContent(value);
-                  }}
-                  className="dark:bg-dark-900"
-                />
-                <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-                  <ChevronDownIcon />
-                </span>
-              </div>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
+      <ComponentCard title="Generate Cover Letter">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (input.trim()) {
+              sendMessage({ text: input });
+              setInput("");
+            }
+          }}
+        >
+          <div className="space-y-4">
+            <div>
+              <Label>Select Job</Label>
+              {getJobsData && getJobsData.getJobs ? (
+                <div className="relative">
+                  <Select
+                    options={getJobsData.getJobs.map((job: Job) => ({
+                      value: job.description,
+                      label: `${job.company} - ${job.role}`,
+                    }))}
+                    placeholder="Select a job"
+                    value={jobDescription || ""}
+                    onChange={(value: string) => {
+                      setJobDescription(value);
+                    }}
+                    className="dark:bg-dark-900"
+                  />
+                  <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+                    <ChevronDownIcon />
+                  </span>
+                </div>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+            <div>
+              <Label>Select Resume</Label>
+              {getResumesData && getResumesData.getResumes ? (
+                <div className="relative">
+                  <Select
+                    options={getResumesData.getResumes.map(
+                      (resume: Resume) => ({
+                        value: resume.content,
+                        label: `${resume.dateCreated}-v${resume.version}`,
+                      })
+                    )}
+                    placeholder="Select a resume"
+                    value={resumeContent || ""}
+                    onChange={(value: string) => {
+                      setResumeContent(value);
+                    }}
+                    className="dark:bg-dark-900"
+                  />
+                  <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+                    <ChevronDownIcon />
+                  </span>
+                </div>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+            <div>
+              <Label>Select Cover Letter Template</Label>
+              {getCoverLetterTemplatesData &&
+              getCoverLetterTemplatesData.getCoverLetterTemplates ? (
+                <div className="relative">
+                  <Select
+                    options={getCoverLetterTemplatesData.getCoverLetterTemplates.map(
+                      (coverLetterTemplate: CoverLetterTemplate) => ({
+                        value: coverLetterTemplate.content,
+                        label: coverLetterTemplate.title,
+                      })
+                    )}
+                    placeholder="Select a cover letter template"
+                    value={coverLetterTemplateContent || ""}
+                    onChange={(value: string) => {
+                      setCoverLetterTemplateContent(value);
+                    }}
+                    className="dark:bg-dark-900"
+                  />
+                  <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
+                    <ChevronDownIcon />
+                  </span>
+                </div>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
 
-          <div className="flex justify-end">
-            <Button size="sm" variant="primary" type="submit">
-              Generate
-            </Button>
+            <div className="flex justify-end">
+              <Button size="sm" variant="primary" type="submit">
+                Generate
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </ComponentCard>
+      {messages.filter((message) => message.role !== "user").length > 0 && (
+        <ComponentCard title="Cover Letter">
+          <Markdown>
+            {messages
+              .filter((message) => message.role !== "user")
+              .map((message) =>
+                message.parts
+                  .filter((part) => part.type === "text")
+                  .map((part) => part.text)
+                  .join(" ")
+              )
+              .join("\n")}
+          </Markdown>
+        </ComponentCard>
+      )}
     </div>
   );
 };
