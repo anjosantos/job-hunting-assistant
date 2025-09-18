e421e45f65b4817f35674636f9c5563d656d1409
Anjo Santos <anjosantos92@gmail.com>
2025-09-18 10:42:34 -0700

    Remove notification

--- client/layout/AppHeader.tsx
+++ client/layout/AppHeader.tsx
@@ -129,9 +129,6 @@ const AppHeader: React.FC = () => {
             {/* <!-- Dark Mode Toggler --> */}
             <ThemeToggleButton />
             {/* <!-- Dark Mode Toggler --> */}
-
-            <NotificationDropdown />
-            {/* <!-- Notification Menu Area --> */}
           </div>
           {/* <!-- User Area --> */}
           <UserDropdown />
